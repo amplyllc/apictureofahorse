012a9d4273a74aae595d3f49ad38113befa2abdf
amplyllc <contact@amply.llc>
2024-12-02 13:16:48 -0500

    added gen horses function

---
+++ src/assets/js/display-image.js
@@ -0,0 +1,26 @@
+const imageArray = [
+	"/assets/img/horse1",
+	"/assets/img/horse2",
+	"/assets/img/horse3",
+	"/assets/img/horse4",
+	"/assets/img/horse5",
+	"/assets/img/horse6",
+	"/assets/img/horse7",
+	"/assets/img/horse8",
+	"/assets/img/horse9",
+	"/assets/img/horse10"
+];
+
+const image = document.querySelector("img");
+const source = document.querySelector("source");
+const button = document.querySelector("button");
+
+window.onload = () => generateRandomPicture(imageArray);
+
+button.addEventListener("click", () => generateRandomPicture(imageArray));
+
+function generateRandomPicture(array){
+	let randomNum = Math.floor(Math.random() * array.length); 
+	image.setAttribute("src", array[randomNum] + "-m" + ".jpg");
+	source.setAttribute("srcset", array[randomNum] + ".jpg");
+}
